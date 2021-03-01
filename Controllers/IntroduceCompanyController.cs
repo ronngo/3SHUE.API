fd5d1a80f805ac0f41626179772de6b6fb922919
ngô rôn <ronngomn23@gmail.com>
2021-03-01 13:06:35 +0700

    API Web

---
+++ Controllers/IntroduceCompanyController.cs
@@ -0,0 +1,28 @@
+﻿using _3SHUE.BAL.Interface.InCompany;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace _3SHUE.API.Controllers
+{
+    [ApiController]
+    public class IntroduceCompanyController : ControllerBase
+    {
+
+        private readonly IIntroduceCompanyService introduceCompany;
+
+        public IntroduceCompanyController(IIntroduceCompanyService introduceCompany)
+        {
+            this.introduceCompany = introduceCompany;
+        }
+
+
+
+        [HttpGet]
+        [Route("/api/introduce/Create")]
+        public async Task<OkObjectResult> Gets()
+        {
+            return Ok(await introduceCompany.Get());
+        }
+    }
+
+}
